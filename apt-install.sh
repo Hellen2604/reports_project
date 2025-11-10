2db1d75fba7036abf6f6e8e093a61c9afe639069
Elex03 <eacuna042@gmail.com>
2025-11-10 08:22:53 -0600

    add: apt-install sh

---
+++ apt-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+# Instala dependencias del sistema
+apt-get update
+apt-get install -y curl gnupg2 apt-transport-https software-properties-common
+
+# Agrega el repositorio de Microsoft
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# Instala el driver ODBC
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
